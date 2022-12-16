e156991fd6b72f53c6e77e997de72165fc001f06
Paul Fournier <paul.fournier.3314@gmail.com>
2022-12-16 11:44:09 +0100

    create migration (create-booking)

---
+++ migrations/02-create-booking.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      post_id: {
+        type: Sequelize.INTEGER
+      },
+      start_date: {
+        type: Sequelize.DATE
+      },
+      end_date: {
+        type: Sequelize.DATE
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookings');
+  }
+};
