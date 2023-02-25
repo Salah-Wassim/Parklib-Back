6994815d6c7ef7f5963136095f38e16e4524f5c1
Salah-Wassim <salahwassim.arfae1@gmail.com>
2023-02-25 10:51:26 +0100

    Booking: added authentication middleware on the creation and deletion route

--- routes/booking.router.js
+++ routes/booking.router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const reservation = require("../controllers/booking.controller.js");
-
+const authenticateJWT = require("../middleware/authjwt.js").authenticateJWT;
 
 router.get("/", reservation.findAllBooking);
 router.get("/find/:id", reservation.findOneBooking);
-router.post("/create", reservation.createBooking);
-router.delete("/:id/delete", reservation.deleteBooking);
+router.post("/create", authenticateJWT, reservation.createBooking);
+router.delete("/:id/delete", authenticateJWT, reservation.deleteBooking);
 // router.put("/update", reservation.updateBooking);
 
 
