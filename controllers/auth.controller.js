56568d7625b238559da894303e43c9efcba96b8a
Salah-Wassim <salahwassim.arfae1@gmail.com>
2023-02-26 14:25:01 +0100

    Auth : Assign the User role when a user registers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models").User;
+const Role = require("../models").Role;
+const RoleUser = require("../models").RoleUser;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const constante = require('../utils/constantes.util.js');
@@ -7,7 +9,7 @@ const HttpStatus = require('../utils/httpStatus.util.js');
 const Response = require('../utils/response.util.js');
 require('dotenv').config();
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
     if (req.body.password == null || req.body.email == null) {
         res.status(HttpStatus.NO_CONTENT.code)
             .send(new Response(HttpStatus.NO_CONTENT.code,HttpStatus.NO_CONTENT.message,`Content can not be empty!` ));
@@ -30,16 +32,87 @@ exports.register = (req, res) => {
         logger.info(`${req.method} ${req.originalUrl}, Creating new user.`);
         // Save User in the database
         User.create(user)
-            .then(data => {
+            .then(async (data) => {
+                if(data){
+                    console.log('data', data)
                     const accessToken = jwt.sign({
                         id: data.id,
                         email: data.email
                     }, process.env.SECRET, { expiresIn:process.env.EXPIRES_IN});
 
+                    const role = await Role.findOne({ where: { title: "User" } });
+                    
+                    if(role){
+                        let roleUser = {};
+                        roleUser = {
+                            UserId: data.id ? data.id : null,
+                            RoleId: role.id ? role.id : null
+                        }
+                        for(value in roleUser){
+                            if(!roleUser[value]){
+                                res.status(HttpStatus.BAD_REQUEST.code).send(
+                                    new Response(
+                                        HttpStatus.BAD_REQUEST.code,
+                                        HttpStatus.BAD_REQUEST.message,
+                                        'Content cannot be empty'
+                                    )
+                                )
+                            }
+                        }
+                        RoleUser.create(roleUser)
+                        .then(response => {
+                            if(response[0]===0){
+                                res.status(HttpStatus.NOT_FOUND.code).send(
+                                    new Response(
+                                        HttpStatus.NOT_FOUND.code,
+                                        HttpStatus.NOT_FOUND.message,
+                                        'Any response for RoleUser was returned'
+                                    )
+                                )
+                            }
+                            res.status(HttpStatus.CREATED.code).send(
+                                new Response(
+                                    HttpStatus.CREATED.code,
+                                    HttpStatus.CREATED.message,
+                                    'RoleUser was created'
+                                )
+                            )
+                        })
+                        .catch(err => {
+                            console.log("err1", err);
+                            res.status(HttpStatus.INTERNAL_SERVER_ERROR.code).send(
+                                new Response(
+                                    HttpStatus.INTERNAL_SERVER_ERROR.code,
+                                    HttpStatus.INTERNAL_SERVER_ERROR.message,
+                                    'An internal error has occurred'
+                                )
+                            )
+                        })
+                    }
+                    else{
+                        res.status(HttpStatus.NOT_FOUND.code).send(
+                            new Response(
+                                HttpStatus.NOT_FOUND.code,
+                                HttpStatus.NOT_FOUND.message,
+                                `Any role ${role.title} was found`
+                            )
+                        )
+                    }
                     res.status(HttpStatus.CREATED.code)
-                        .send(new Response(HttpStatus.CREATED.code,HttpStatus.CREATED.message,`Account created`, {accessToken}));
-                })
+                    .send(new Response(HttpStatus.CREATED.code,HttpStatus.CREATED.message,`Account created`, {accessToken}));
+                }
+                else{
+                    res.status(HttpStatus.NOT_FOUND.code).send(
+                        new Response(
+                            HttpStatus.NOT_FOUND.code,
+                            HttpStatus.NOT_FOUND.message,
+                            'Any response user was returned'
+                        )
+                    )
+                }
+            })
             .catch (error => {
+                console.log("error1", error)
                 if (error.name === 'SequelizeUniqueConstraintError') {
                     res.status(HttpStatus.CONFLICT.code)
                         .send(new Response(HttpStatus.CONFLICT.code,HttpStatus.CONFLICT.message,`Account already exists` ));
