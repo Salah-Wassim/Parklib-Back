20662173c125a9507c221cd57351188a2383dede
Ken B <kenb9027@gmail.com>
2023-03-31 09:58:41 +0200

    update router with Paul's work too

--- routes/picture.router.js
+++ routes/picture.router.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const postPicture = require("../controllers/post_picture.controller.js");
 
 
-router.post("/upload", postPicture.uploadPostPicture);
-router.get("/:id", postPicture.getPostPicture);
-router.put("/:id", postPicture.updatePostPicture);
-router.delete("/:id", postPicture.deletePostPicture);
+// Post Picture
+router.post("/post/upload", postPicture.uploadPostPicture);
+router.get("/post/:id", postPicture.getPostPicture);
+router.put("/post/:id", postPicture.updatePostPicture);
+router.delete("/post/:id", postPicture.deletePostPicture);
 
 
 
