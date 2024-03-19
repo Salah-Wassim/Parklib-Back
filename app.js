98a186b401d07f9d969681b1dcb82e7716259d85
Salah-Wassim <salahwassim.arfae1@gmail.com>
2024-03-19 18:49:08 +0100

    Setting up the CORS configuration

--- app.js
+++ app.js
@@ -36,6 +36,18 @@ const authenticateJWT = require("./middleware/authjwt.js").authenticateJWT;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: true, // "true" will copy the domain of the request back
+                  // to the reply. If you need more control than this
+                  // use a function.
+    credentials: true, // This MUST be "true" if your endpoint is
+                       // authenticated via either a session cookie
+                       // or Authorization header. Otherwise the
+                       // browser will block the response.
+    methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+                                           // pre-flight OPTIONS requests
+}));
+
 app.use(cors({origin:'*'}));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
